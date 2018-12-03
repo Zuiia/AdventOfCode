13411461fd2423df7ab1f5a157d23fbd0c062cdf
Caja <caja-sophie.jakobs@gmx.de>
2018-12-03 23:38:43 +0100

    Caja DayTwo PartTwo finished

--- Caja/src/DayTwo/DayTwo.java
+++ Caja/src/DayTwo/DayTwo.java
@@ -40,4 +40,43 @@ public class DayTwo {
         }
         return count;
     }
+
+    public static String rightID(ArrayList<String> input) {
+        String sameChars = "";
+        ArrayList<String> nearlyTwins = new ArrayList<>();
+        for (String first : input) {
+            for (String second : input) {
+                if (nearlySame(first, second)) {
+                    nearlyTwins.add(first);
+                    nearlyTwins.add(second);
+                }
+            }
+        }
+
+        // BEWARE This is ugly and hardcoded ( i don't know why every word is doubled in the arry, i guess something with the for-loops but I'm to lazy to check...
+        for (int i = 0; i < nearlyTwins.get(1).length(); i++) {
+            if ((nearlyTwins.get(1).charAt(i) == nearlyTwins.get(0).charAt(i))) {
+                sameChars += nearlyTwins.get(1).charAt(i);
+            }
+        }
+        System.out.println(nearlyTwins);
+        return sameChars;
+    }
+
+    public static boolean nearlySame (String first, String second) {
+        int difference = 0;
+
+        for (int i = 0; i < first.length(); i++) {
+            if (!(first.charAt(i) == second.charAt(i))) {
+                difference++;
+                if (difference > 1) {
+                    return false;
+                }
+            }
+        }
+        if (difference == 1) {
+            return true;
+        }
+        return false;
+    }
 }
--- Caja/src/Main.java
+++ Caja/src/Main.java
@@ -20,6 +20,7 @@ public class Main {
         System.out.println(data);
 
         System.out.println(DayTwo.checksum(data));
+        System.out.println(DayTwo.rightID(data));
     }
 }
 
