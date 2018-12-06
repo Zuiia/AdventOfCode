644747171f21df76869dece21ddb196f875fb173
Jonas Gonka <jonas.gonka@mni.thm.de>
2018-12-06 11:20:39 +0100

    Arbeit an Part 2

--- Jonas/Day5/Day5.java
+++ Jonas/Day5/Day5.java
@@ -7,16 +7,23 @@ public class Day5 {
         JFileReader reader = new JFileReader("Day5/input.txt");
         String polymers = reader.getText();
 
-        System.out.println("Part 1: " + destroy(polymers.replaceAll("\\s+","")));
+        //System.out.println("Part 1: " + destroy(polymers.replaceAll("\\s+",""), '.'));
+
+        System.out.println("Part 2: " + optimize(polymers.replaceAll("\\s+", "")));
     }
 
-    private static int destroy (String polymers) {
+    private static int destroy (String polymers, char run) {
         StringBuilder polyBomb = new StringBuilder(polymers);
 
         boolean complete = false;
 
+
+        int parse = 0;
+
         while (!complete) {
             complete = true;
+
+            System.out.println("Run " + run + ", Parse " + parse++ + " starting...");
 
             for (int i = 0; i < polyBomb.length() - 1;) {
                 if (react(polyBomb.toString(), i)) {
@@ -29,7 +36,6 @@ public class Day5 {
 
         }
 
-        System.out.println(polyBomb.toString());
         return polyBomb.length();
     }
 
@@ -42,5 +48,24 @@ public class Day5 {
         }
 
         return false;
+    }
+
+    private static int optimize (String polymere) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int optLen = Integer.MAX_VALUE;
+        int optChar;
+
+        for (char c : alphabet.toCharArray()) {
+            String regex = "/" + Character.toLowerCase(c) + "|" + Character.toUpperCase(c) + "/g";
+            String test = polymere.replaceAll(regex, "");
+            System.out.println(regex + " " + polymere.replaceAll("/A|a/g", ""));
+            int len = destroy(polymere.replaceAll(regex, ""), c);
+            if ( len < optLen ) {
+                optLen = len;
+                optChar = c;
+            }
+        }
+
+        return optLen;
     }
 }
